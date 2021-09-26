67270414e113c1d8c0e66450eb7bad49177dbfda
nghianguyen119 <trongnghia.nguyen@outlook.com>
2021-09-26 16:29:28 +0200

    Warning in homepage

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import Router from './routes';
 import ThemeConfig from './theme';
 import GlobalStyles from './theme/globalStyles';
 // components
-import Settings from './components/settings';
 import RtlLayout from './components/RtlLayout';
 import ScrollToTop from './components/ScrollToTop';
 import { ProgressBarStyle } from './components/LoadingScreen';
 import ThemePrimaryColor from './components/ThemePrimaryColor';
+import NotistackProvider from './components/NotistackProvider';
 
 // ----------------------------------------------------------------------
 
@@ -17,10 +17,12 @@ export default function App() {
     <ThemeConfig>
       <ThemePrimaryColor>
         <RtlLayout>
-          <GlobalStyles />
-          <ProgressBarStyle />
-          <ScrollToTop />
-          <Router />
+          <NotistackProvider>
+            <GlobalStyles />
+            <ProgressBarStyle />
+            <ScrollToTop />
+            <Router />
+          </NotistackProvider>
         </RtlLayout>
       </ThemePrimaryColor>
     </ThemeConfig>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
 // material
-import { Container, Grid } from '@mui/material';
+import { useEffect } from 'react';
+import { Container, Grid, Typography, IconButton } from '@mui/material';
+
+import { Icon } from '@iconify/react';
+import closeFill from '@iconify/icons-eva/close-fill';
+import { useSnackbar, VariantType, SnackbarOrigin } from 'notistack';
+
 // hooks
 import useSettings from '../hooks/useSettings';
 // components
@@ -8,6 +14,32 @@ import { HomeWelcome, HomeNftPresentation } from '../components/_dashboard/home'
 // ----------------------------------------------------------------------
 export default function Home() {
   const { themeStretch } = useSettings();
+
+  useEffect(() => {
+    onSnackbarClose('info');
+  }, []);
+
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const onSnackbarClose = (color: VariantType) => {
+    enqueueSnackbar(
+      <div>
+        <Typography variant="subtitle2" sx={{ textTransform: 'capitalize' }}>
+          {color}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Switchswap didn't issue any token
+        </Typography>
+      </div>,
+      {
+        variant: color,
+        action: (key) => (
+          <IconButton size="small" color="inherit" onClick={() => closeSnackbar(key)}>
+            <Icon icon={closeFill} width={24} height={24} />
+          </IconButton>
+        )
+      }
+    );
+  };
 
   return (
     <Page title="Home">
