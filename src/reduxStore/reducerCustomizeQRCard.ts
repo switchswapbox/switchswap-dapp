545a4be69f311e0194c70f21813a3a8b9e801256
Huy Dung NGUYEN <huydung.ng.179@gmail.com>
2021-10-13 23:31:24 +0200

    add missing files

---
+++ src/reduxStore/reducerCustomizeQRCard.ts
@@ -0,0 +1,29 @@
+export const CUSTOMIZE_QR_CARD = 'CUSTOMIZE_QR_CARD';
+
+export interface InfoQRCard {
+  layout: string;
+}
+
+export const changeQRCard = (infoQRCard: InfoQRCard) => ({
+  type: CUSTOMIZE_QR_CARD,
+  infoQRCard: infoQRCard
+});
+
+// init state
+const initialQRCard: InfoQRCard = {
+  layout: 'svg1'
+};
+
+export const qrCardReducer = (
+  state = initialQRCard,
+  action: { type: string; infoQRCard: InfoQRCard }
+) => {
+  switch (action.type) {
+    case CUSTOMIZE_QR_CARD:
+      return {
+        layout: action.infoQRCard.layout
+      };
+    default:
+      return state;
+  }
+};
