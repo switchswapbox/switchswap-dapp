1a04b8d6793cad35738b4fddfde94b4a99aa9fbd
Huy Dung NGUYEN <huydung.ng.179@gmail.com>
2021-10-15 23:30:41 +0200

    Split minting step

--- src/components/_dashboard/nftMinting/MintingProcess.tsx
+++ src/components/_dashboard/nftMinting/MintingProcess.tsx
@@ -3,40 +3,24 @@ import React, { useState, useEffect } from 'react';
 import {
   Box,
   Step,
-  Divider,
   Paper,
-  Zoom,
-  SvgIcon,
   Button,
-  Stack,
-  ToggleButton,
-  ToggleButtonGroup,
-  Tooltip,
-  Grid,
   IconButton,
   Stepper,
   StepLabel,
-  Typography,
-  LinearProgress,
-  Alert
+  Typography
 } from '@mui/material';
 
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import Label from '../../Label';
 import { useSnackbar, VariantType } from 'notistack';
 import closeFill from '@iconify/icons-eva/close-fill';
 
 import Scrollbar from '../../Scrollbar';
-import detectEthereumProvider from '@metamask/detect-provider';
-import { ethers } from 'ethers';
 import { Icon } from '@iconify/react';
 
-import { ABI } from '../../../utils/abi';
-import { contractAddress } from '../../../utils/contractAddress';
-import { IPFS_GATEWAY_W3AUTH } from '../../../assets/COMMON_VARIABLES';
 import UploadFileStep, { FileInfoType } from './mintingSteps/StepUploadFile';
 import StepCustomizeNFTCard from './mintingSteps/StepCustomizeNFTCard';
 import { MintingContext, MintingContextInterface } from './minting.context';
+import StepMintNFT from './mintingSteps/StepMintNFT';
 
 // ----------------------------------------------------------------------
 const steps = ['Upload File', 'Customize NFT Card', 'Mint NFT'];
@@ -143,11 +127,6 @@ export default function MintingProcess({ nftType }: MintingProcessProps) {
     setActiveStep(0);
   };
 
-  const [transactionHash, setTransactionHash] = useState('');
-  const [isMinting, setMinting] = useState(false);
-  const [nftMinted, setNftMinted] = useState(false);
-  const [tokenID, setTokenID] = useState(0);
-
   const loadImg = () => {
     const reader = new FileReader();
 
@@ -162,40 +141,6 @@ export default function MintingProcess({ nftType }: MintingProcessProps) {
       loadImg();
     }
   }, [files[0]]);
-
-  async function mintDataNTF() {
-    const provider = await detectEthereumProvider();
-    if (provider && provider.isMetaMask) {
-      const chainId = await provider.request({
-        method: 'eth_chainId'
-      });
-
-      if (parseInt(chainId, 16) === 137) {
-        setMinting(true);
-        const providerEthers = new ethers.providers.Web3Provider(provider);
-        const signer = providerEthers.getSigner();
-        const addr = await signer.getAddress();
-        const contract = new ethers.Contract(contractAddress, ABI, providerEthers);
-        const signedContract = contract.connect(signer);
-        signedContract
-          .mintDataNTF(addr, `ipfs://${metadataCid}`, `ipfs://${uploadedCid.cid}`, 'null')
-          .then((tx: any) => {
-            setTransactionHash(tx.hash);
-            providerEthers.waitForTransaction(tx.hash).then(() => {
-              setMinting(false);
-              setNftMinted(true);
-              providerEthers.getTransactionReceipt(tx.hash).then((receipt: any) => {
-                setTokenID(parseInt(receipt.logs[0].topics[3], 16));
-              });
-            });
-          })
-          .catch((error: any) => {
-            console.log(error);
-            setMinting(false);
-          });
-      }
-    }
-  }
 
   return (
     <MintingContext.Provider value={initMintingContext}>
@@ -298,144 +243,7 @@ export default function MintingProcess({ nftType }: MintingProcessProps) {
 
       {activeStep === 2 ? (
         <>
-          <Grid container spacing={3} sx={{ pt: 5 }}>
-            <Grid item xs={12} md={6} lg={7}>
-              {/* <ProductDetailsCarousel product={product} /> */}
-              <Stack alignItems="center" justifyContent="center">
-                <Box sx={{ borderRadius: 2 }} component="img" src={srcImage} />
-              </Stack>
-            </Grid>
-            <Grid item xs={12} md={6} lg={5}>
-              <Label variant="ghost" color="success" sx={{ textTransform: 'uppercase' }}>
-                Minting NFT
-              </Label>
-              <Typography
-                variant="h5"
-                paragraph
-                sx={{
-                  mt: 2,
-                  mb: 0
-                }}
-              >
-                {nameNft}
-              </Typography>
-
-              <Box sx={{ mt: 2, display: 'flex', alignItems: 'center' }}>
-                <Typography variant="body1" sx={{ color: 'text.secondary' }}>
-                  {descNft}
-                </Typography>
-              </Box>
-            </Grid>
-          </Grid>
-
-          {isMinting ? <LinearProgress color="info" sx={{ my: 3 }} /> : <Divider sx={{ my: 3 }} />}
-          <Grid
-            container
-            sx={{
-              // borderRadius: 1,
-              // border: (theme) => `solid 1px ${theme.palette.divider}`,
-              bgcolor: 'background.paper'
-            }}
-            spacing={1}
-          >
-            <Grid item xs={12} md={3}>
-              <Stack direction="row" alignItems="center" spacing={2} sx={{ height: '100%' }}>
-                <Typography variant="h6">Mint NFT</Typography>
-                <Tooltip
-                  TransitionComponent={Zoom}
-                  title="You must have a small amount of token in your wallet"
-                >
-                  <HelpOutlineIcon />
-                </Tooltip>
-              </Stack>
-            </Grid>
-            <Grid item xs={12} md={9}>
-              <Stack
-                direction="row"
-                alignItems="center"
-                justifyContent={{ xs: 'flex-start', md: 'flex-end' }}
-              >
-                <Button
-                  size="large"
-                  type="button"
-                  color="warning"
-                  variant="contained"
-                  onClick={mintDataNTF}
-                  startIcon={
-                    <Box
-                      component="img"
-                      src="./static/icons/shared/polygon.svg"
-                      sx={{ height: '24px', width: '32px' }}
-                    />
-                  }
-                  sx={{ whiteSpace: 'nowrap' }}
-                >
-                  Mint NFT
-                </Button>
-              </Stack>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} sx={{ pt: 3, display: transactionHash !== '' ? 'flex' : 'none' }}>
-            <Alert
-              icon={false}
-              severity={nftMinted ? 'success' : 'info'}
-              sx={{
-                width: '100%',
-                wordBreak: 'break-word'
-              }}
-            >
-              {nftMinted
-                ? `Your NFT is minted with transaction hash: ${transactionHash} `
-                : `Your request of minting NFT is in progress with transaction hash: ${transactionHash} `}
-              <SvgIcon>
-                <Icon icon="fxemoji:rocket" />
-              </SvgIcon>
-            </Alert>
-          </Grid>
-
-          {isMinting ? (
-            <LinearProgress variant="query" color="info" sx={{ my: 3 }} />
-          ) : (
-            <Divider sx={{ my: 3 }} />
-          )}
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent={{ xs: 'center' }}
-            sx={{ pb: 3, width: '100%', display: transactionHash === '' ? 'none' : 'flex' }}
-          >
-            <ToggleButtonGroup value={alignment} exclusive onChange={handleAlignment}>
-              <ToggleButton
-                value="etherscan"
-                sx={{ minWidth: '56px', display: transactionHash === '' ? 'none' : 'flex' }}
-                href={transactionHash !== '' ? `https://polygonscan.com/tx/${transactionHash}` : ''}
-                target="_blank"
-              >
-                <Box
-                  component="img"
-                  src="./static/icons/shared/polygon-white.svg"
-                  sx={{ height: '24px', width: '32px' }}
-                />
-              </ToggleButton>
-              <ToggleButton
-                value="opensea"
-                sx={{ minWidth: '56px', display: tokenID === 0 ? 'none' : 'flex' }}
-                href={
-                  tokenID !== 0
-                    ? `https://opensea.io/assets/matic/${contractAddress}/${tokenID}`
-                    : ''
-                }
-                target="_blank"
-              >
-                <Box
-                  component="img"
-                  src="./static/icons/shared/opensea.svg"
-                  sx={{ height: '24px', width: '32px' }}
-                />
-              </ToggleButton>
-            </ToggleButtonGroup>
-          </Stack>
-
+          <StepMintNFT handleAlignment={handleAlignment} />
           <Box sx={{ display: 'flex' }}>
             <Button color="inherit" onClick={handleBack} sx={{ mr: 1 }}>
               Back
