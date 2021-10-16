c587e7633202e49788430b62021d4e70015ba460
nghianguyen119 <trongnghia.nguyen@outlook.com>
2021-10-16 03:21:48 +0200

    Fetching sample

--- src/pages/NftManager.tsx
+++ src/pages/NftManager.tsx
@@ -12,20 +12,16 @@ import { contractAddress } from '../utils/contractAddress';
 import { Icon } from '@iconify/react';
 import roundVpnKey from '@iconify/icons-ic/round-vpn-key';
 import peopleFill from '@iconify/icons-eva/people-fill';
+import axios from 'axios';
 // ----------------------------------------------------------------------
 
 type BookingItemProps = {
-  id: string;
-  name: string;
-  avatar: string;
-  bookdAt: Date | string | number;
-  roomNumber: string;
-  person: string;
-  roomType: string;
-  cover: string;
+  tokenId: string;
+  tokenURI: string;
+  imageURI: string;
 };
 
-function BookingItem() {
+function BookingItem({ tokenId, tokenURI, imageURI }: BookingItemProps) {
   // const { avatar, name, roomNumber, bookdAt, person, cover, roomType } = item;
 
   return (
@@ -45,11 +41,7 @@ function BookingItem() {
       </Stack>
 
       <Box sx={{ p: 1, position: 'relative' }}>
-        <Box
-          component="img"
-          src="https://ipfs.io/ipfs/QmUWzLqn3zkWgBqxWg4Dd71JzgCb7Ft46grqXkBwyqGqtH"
-          sx={{ borderRadius: 1.5, width: 1 }}
-        />
+        <Box component="img" src={imageURI} sx={{ borderRadius: 1.5, width: 1 }} />
       </Box>
     </Paper>
   );
@@ -58,14 +50,39 @@ function BookingItem() {
 export default function PageFive() {
   const { themeStretch } = useSettings();
   const [totalNFT, setTotalNFT] = useState<number>(0);
-  const [NftList, setNftList] = useState<{ tokenId: string; tokenURI: string }[]>([]);
+  const [NftList, setNftList] = useState<{ tokenId: string; tokenURI: string; imageURI: string }[]>(
+    []
+  );
   // WARNING: Use useSth to prevent re-create for each render
 
   const [selectedMetamaskAccount, setselectedMetamaskAccount] = useState(
     localStorage.getItem('selectedMetamaskAccount') || ''
   );
 
-  setNftList([{ tokenId: '1' }]);
+  useEffect(() => {
+    async function getAllNft() {
+      const provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/');
+      const contract = new ethers.Contract(contractAddress, ABI, provider);
+      const a = (await contract.balanceOf(selectedMetamaskAccount)).toString();
+      // for (let index = 0; index < parseInt(a, 10); index++) {
+      for (let index = 0; index < 1; index++) {
+        const tokenId = (
+          await contract.tokenOfOwnerByIndex(selectedMetamaskAccount, index)
+        ).toString();
+        const tokenURI = await contract.tokenURI(tokenId);
+        axios
+          .get('https://ipfs.io/ipfs/QmV1BcaBUZLRLXkYuYYBXMpGBoXSwtXtogQhfkpTuEi9jV')
+          .then((response) => {
+            console.log(response.data);
+            const imageURI = 'https://ipfs.io/ipfs/QmYVSo2YxDnVruJ9QYvrdosTygdGwzt2TqURa1y9Fh6NUq';
+            NftList.push({ tokenId, tokenURI, imageURI });
+            setNftList([...NftList]);
+            console.log(NftList);
+          });
+      }
+    }
+    getAllNft();
+  }, []);
 
   const getTotal = async () => {
     const provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/');
@@ -76,7 +93,8 @@ export default function PageFive() {
         await contract.tokenOfOwnerByIndex(selectedMetamaskAccount, index)
       ).toString();
       const tokenURI = await contract.tokenURI(tokenId);
-      NftList.push({ tokenId, tokenURI });
+      const imageURI = '';
+      NftList.push({ tokenId, tokenURI, imageURI });
       setNftList([...NftList]);
       // console.log(tokenId);
       // console.log(tokenURI);
@@ -89,12 +107,13 @@ export default function PageFive() {
       <Container maxWidth={themeStretch ? false : 'xl'}>
         <Button onClick={getTotal}>Hello</Button>
         <Grid container spacing={3}>
-          <Grid key="12" item xs={12} sm={6} md={4}>
-            <BookingItem />
-          </Grid>
-          <Grid key="12" item xs={12} sm={6} md={4}>
-            <BookingItem />
-          </Grid>
+          {NftList.map((nft) => {
+            return (
+              <Grid key="12" item xs={12} sm={6} md={4}>
+                <BookingItem {...nft} />
+              </Grid>
+            );
+          })}
         </Grid>
       </Container>
     </Page>
