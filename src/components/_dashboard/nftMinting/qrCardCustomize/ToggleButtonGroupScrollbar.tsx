12e1d49099bde04345c10bea7492cd4841830142
Huy Dung NGUYEN <huydung.ng.179@gmail.com>
2021-10-28 04:15:47 +0200

    select style

--- src/components/_dashboard/nftMinting/qrCardCustomize/ToggleButtonGroupScrollbar.tsx
+++ src/components/_dashboard/nftMinting/qrCardCustomize/ToggleButtonGroupScrollbar.tsx
@@ -1,19 +1,42 @@
 import { Box, Stack, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import { SxProps } from '@mui/system';
 import Scrollbar from 'components/Scrollbar';
 import React from 'react';
 
-const singleButton = (
+const successColor = '#E9FCD4';
+const infoColor = '#D0F2FF';
+
+const useStyles = makeStyles({
+  root: {
+    '&.Mui-selected': {
+      backgroundColor: successColor,
+      '&:hover': {
+        backgroundColor: successColor
+      }
+    },
+    '&:hover': {
+      backgroundColor: infoColor
+    },
+    '&:active': {
+      backgroundColor: successColor
+    }
+  }
+});
+
+const SingleButton = (
   name: string | number,
   src: string,
   handleSelect: (name: string | number) => void,
   buttonDim: string
 ) => {
+  const classes = useStyles();
   return (
     <ToggleButton
       key={name}
       value={name}
       onClick={() => handleSelect(name)}
+      className={classes.root}
       sx={{ padding: '7px' }}
     >
       <Box
@@ -71,7 +94,7 @@ const ToggleButtonGroupScrollbar = ({
           <Scrollbar>
             <ToggleButtonGroup value={value} exclusive>
               {nameArray.map((name, index) =>
-                singleButton(name, srcArray[index], handleSelect, buttonDim)
+                SingleButton(name, srcArray[index], handleSelect, buttonDim)
               )}
             </ToggleButtonGroup>
           </Scrollbar>
