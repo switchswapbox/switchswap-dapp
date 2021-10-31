885d9c1f0e7a5d913e98a668b69ccf51424b738f
nghianguyen119 <trongnghia.nguyen@outlook.com>
2021-10-31 02:13:15 +0200

    Adding prepaid

--- src/components/_dashboard/assetViewer/FilesInfo.tsx
+++ src/components/_dashboard/assetViewer/FilesInfo.tsx
@@ -18,7 +18,17 @@ import {
   TableHead,
   Typography,
   CardHeader,
-  TableContainer
+  TableContainer,
+  DialogProps,
+  Button,
+  Dialog,
+  Box,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  TextField,
+  InputAdornment
 } from '@mui/material';
 // utils
 import Label from '../../Label';
@@ -124,8 +134,7 @@ const addPrepaid = async (cid: string, amount: number) => {
   });
 
   chain.disconnect();
-  console.log(`txHash ${txHash}`);
-  console.log(txHash);
+
   return txHash;
 };
 
@@ -133,6 +142,7 @@ function MoreMenuButton({ cid, fileSize }: { cid: string; fileSize: number }) {
   const menuRef = useRef(null);
   const [open, setOpen] = useState(false);
   const onSnackbarAction = useSnackbarAction();
+  const [addPrepaidOpen, setAddPrepaidOpen] = useState(false);
 
   const handleOpen = () => {
     setOpen(true);
@@ -153,7 +163,13 @@ function MoreMenuButton({ cid, fileSize }: { cid: string; fileSize: number }) {
         `https://crust.subscan.io/extrinsic/${txHash}`
       );
     } else {
-      onSnackbarAction('warning', 'Please install Crust Wallet', null, 'LEARN', CRUST_WALLET_WIKI);
+      onSnackbarAction(
+        'warning',
+        'Please verify your Crust Wallet',
+        null,
+        'LEARN',
+        CRUST_WALLET_WIKI
+      );
     }
   };
 
@@ -178,6 +194,7 @@ function MoreMenuButton({ cid, fileSize }: { cid: string; fileSize: number }) {
         <MenuItem
           onClick={() => {
             handleRenewFile(cid, fileSize);
+            handleClose();
           }}
         >
           <Icon icon="ic:baseline-autorenew" width={20} height={20} />
@@ -187,7 +204,8 @@ function MoreMenuButton({ cid, fileSize }: { cid: string; fileSize: number }) {
         </MenuItem>
         <MenuItem
           onClick={() => {
-            addPrepaid(cid, 0.0001 * 10 ** 12);
+            setAddPrepaidOpen(true);
+            handleClose();
           }}
         >
           <Icon icon="carbon:add-alt" width={20} height={20} />
@@ -210,6 +228,109 @@ function MoreMenuButton({ cid, fileSize }: { cid: string; fileSize: number }) {
           </Typography>
         </MenuItem>
       </Menu>
+
+      <AddPrepaidDialog
+        cid={cid}
+        addPrepaidOpen={addPrepaidOpen}
+        setAddPrepaidOpen={setAddPrepaidOpen}
+      />
+    </>
+  );
+}
+
+export function AddPrepaidDialog({
+  cid,
+  addPrepaidOpen,
+  setAddPrepaidOpen
+}: {
+  cid: string;
+  addPrepaidOpen: boolean;
+  setAddPrepaidOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const [fullWidth, setFullWidth] = useState(true);
+  const [maxWidth, setMaxWidth] = useState<DialogProps['maxWidth']>('sm');
+  const [prepaidAmountRaw, setPrepaidAmountRaw] = useState('');
+  const [prepaidAmount, setPrepaidAmount] = useState(0);
+  const [prepaidAmountError, setPrepaidAmountError] = useState(false);
+  const onSnackbarAction = useSnackbarAction();
+
+  const handleClose = () => {
+    setAddPrepaidOpen(false);
+  };
+
+  const handleConfirm = async () => {
+    if (!prepaidAmountError) {
+      setAddPrepaidOpen(false);
+      const txHash = await addPrepaid(cid, prepaidAmount * 10 ** 12);
+      if (txHash) {
+        onSnackbarAction(
+          'success',
+          'Successfully add prepaid',
+          null,
+          'SUBSCAN',
+          `https://crust.subscan.io/extrinsic/${txHash}`
+        );
+      } else {
+        onSnackbarAction(
+          'warning',
+          'Please verify your Crust Wallet',
+          null,
+          'LEARN',
+          CRUST_WALLET_WIKI
+        );
+      }
+    }
+  };
+
+  const handlePrepaidAmountRaw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrepaidAmountRaw(e.target.value);
+    if (isNaN(e.target.value as any) || isNaN(parseFloat(e.target.value))) {
+      setPrepaidAmountError(true);
+    } else {
+      setPrepaidAmountError(false);
+      setPrepaidAmount(parseFloat(e.target.value));
+    }
+  };
+
+  return (
+    <>
+      <Dialog open={addPrepaidOpen} maxWidth={maxWidth} onClose={handleClose} fullWidth={fullWidth}>
+        <DialogTitle>Amount of prepaid in CRU</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            The file will be automatically renewed with the prepaid fund
+          </DialogContentText>
+
+          <Box
+            sx={{
+              pt: 2,
+              margin: 'auto',
+              display: 'flex',
+              width: '100%',
+              flexDirection: 'column'
+            }}
+          >
+            <TextField
+              error={prepaidAmountError}
+              helperText={prepaidAmountError ? 'Please enter a valid number' : null}
+              id="amout-CRU-prepaid"
+              value={prepaidAmountRaw}
+              onChange={handlePrepaidAmountRaw}
+              InputProps={{
+                endAdornment: <InputAdornment position="start">CRU</InputAdornment>
+              }}
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} variant="contained">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirm} variant="contained">
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
@@ -241,8 +362,6 @@ export default function FilesInfo({ assetAndOwner }: { assetAndOwner: AssetAndOw
       )
         .toISOString()
         .split('T')[0];
-
-      console.log(fileInfo);
 
       setFilesInfo((filesInfo) => {
         const newFileInfo = {
@@ -281,6 +400,7 @@ export default function FilesInfo({ assetAndOwner }: { assetAndOwner: AssetAndOw
   return (
     <Card>
       <CardHeader title="Files Status" sx={{ mb: 3 }} />
+
       <Scrollbar>
         <TableContainer sx={{ minWidth: 700 }}>
           <Table>
