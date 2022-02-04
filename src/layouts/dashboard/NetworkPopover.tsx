db08b74a4f8777198a7f160cc815ede3da5ab2df
nghianguyen119 <trongnghia.nguyen@outlook.com>
2022-02-04 11:41:41 +0100

    Adding hook for network selector

--- src/layouts/dashboard/NetworkPopover.tsx
+++ src/layouts/dashboard/NetworkPopover.tsx
@@ -1,109 +1,57 @@
-import { Icon } from '@iconify/react';
-import { useRef, useState, useEffect } from 'react';
-import { useAppSelector } from '../../redux/hook';
-import messagCircleOutline from '@iconify/icons-eva/message-circle-outline';
-import externaLinkOutline from '@iconify/icons-eva/external-link-outline';
-import { Link as RouterLink } from 'react-router-dom';
+import { useRef, useState } from 'react';
 // material
-import { Avatar, Button, Box, Divider, MenuItem, Typography, Link } from '@mui/material';
+import { Box, MenuItem, ListItemIcon, ListItemText } from '@mui/material';
 // components
-import { MIconButton } from '../../components/@material-extend';
 import MenuPopover from '../../components/MenuPopover';
-
-import { shortenAddress } from '../../utils/formatAddress';
-import useLocales from '../../hooks/useLocales';
-import Identicons from '@nimiq/identicons';
-Identicons.svgPath = './static/identicons.min.svg';
-
+import { MIconButton } from '../../components/@material-extend';
+import useNetworks from '../../hooks/useNetworks';
 // ----------------------------------------------------------------------
 
-export default function AccountPopover() {
+export default function LanguagePopover() {
   const anchorRef = useRef(null);
   const [open, setOpen] = useState(false);
-  const { translate } = useLocales();
-  const selectedAccountAddress = useAppSelector(
-    (state) => state.reducerSelectAccount.accountAddress
-  );
-  const selectedNetworkName = useAppSelector((state) => state.reducerSelectAccount.networkName);
-
-  const [uniqueIcon, setUniqueIcon] = useState<string>('');
-  useEffect(() => {
-    Identicons.toDataUrl(
-      selectedAccountAddress === '' ? 'Hello World' : selectedAccountAddress
-    ).then((img: string) => {
-      setUniqueIcon(img);
-    });
-  }, [selectedAccountAddress]);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const { handleChangeNetwork, currentNetwork, allNetworks } = useNetworks();
 
   return (
     <>
       <MIconButton
         ref={anchorRef}
-        onClick={handleOpen}
+        onClick={() => setOpen(true)}
         sx={{
           padding: 0,
 
-          height: 44,
-          ...(open && {
-            '&:before': {
-              zIndex: 1,
-              content: "''",
-              width: '100%',
-              height: '100%',
-              borderRadius: '50%',
-              position: 'absolute',
-              bgcolor: 'transparent'
-            }
-          })
+          ...(open && { bgcolor: 'action.selected' })
         }}
       >
-        <Typography>Ethereum</Typography>
+        <Box
+          component="img"
+          src={currentNetwork.icon}
+          alt={currentNetwork.label}
+          sx={{ height: '36px' }}
+        />
       </MIconButton>
 
-      <MenuPopover
-        open={open}
-        onClose={handleClose}
-        anchorEl={anchorRef.current}
-        sx={{ width: 220 }}
-      >
-        <MenuItem key="Explorer" sx={{ typography: 'body2', py: 1, px: 2.5 }}>
-          <Box
-            component={Icon}
-            icon={externaLinkOutline}
-            sx={{
-              mr: 2,
-              width: 24,
-              height: 24
-            }}
-          />
-          {translate(`dashboard.explore`)}
-        </MenuItem>
-
-        <MenuItem
-          key="Support"
-          to="#"
-          component={RouterLink}
-          onClick={handleClose}
-          sx={{ typography: 'body2', py: 1, px: 2.5 }}
-        >
-          <Box
-            component={Icon}
-            icon={messagCircleOutline}
-            sx={{
-              mr: 2,
-              width: 24,
-              height: 24
-            }}
-          />
-          {translate(`dashboard.support`)}
-        </MenuItem>
+      <MenuPopover open={open} onClose={() => setOpen(false)} anchorEl={anchorRef.current}>
+        <Box sx={{ py: 1 }}>
+          {allNetworks.map((option) => (
+            <MenuItem
+              key={option.value}
+              selected={option.value === currentNetwork.value}
+              onClick={() => {
+                handleChangeNetwork(option.value);
+                setOpen(false);
+              }}
+              sx={{ py: 1, px: 2.5 }}
+            >
+              <ListItemIcon>
+                <Box component="img" alt={option.label} src={option.icon} sx={{ height: '30px' }} />
+              </ListItemIcon>
+              <ListItemText primaryTypographyProps={{ variant: 'body2' }}>
+                {option.label}
+              </ListItemText>
+            </MenuItem>
+          ))}
+        </Box>
       </MenuPopover>
     </>
   );
