39f488d8773a19cdbbbc71e8a8e49206b800c229
nghianguyen119 <trongnghia.nguyen@outlook.com>
2022-02-10 22:18:28 +0100

    CRUS-47 Add description & preview mock for collection

--- src/pages/CollectionViewer/CollectionViewer.tsx
+++ src/pages/CollectionViewer/CollectionViewer.tsx
@@ -1,13 +1,40 @@
 import { capitalCase } from 'change-case';
 import { useState } from 'react';
 import { styled } from '@mui/material/styles';
-import { Tab, Box, Paper, Card, Tabs, Container, Stack, Grid, Pagination } from '@mui/material';
+import { Icon } from '@iconify/react';
+import twitterFill from '@iconify/icons-eva/twitter-fill';
+import facebookFill from '@iconify/icons-eva/facebook-fill';
+import linkedinFill from '@iconify/icons-eva/linkedin-fill';
+import instagramFilled from '@iconify/icons-ant-design/instagram-filled';
+
+import {
+  Tab,
+  Box,
+  Paper,
+  Card,
+  Tabs,
+  Container,
+  Stack,
+  Grid,
+  Pagination,
+  Typography,
+  Link,
+  Chip
+} from '@mui/material';
 import Page from '../../components/Page';
 import ProfileCover from './components/ProfileCover';
 import { LineScalePulseOutRapid } from 'react-pure-loaders';
 import { contractAddress } from '../../utils/contractAddress';
 
 import NftCard from '../../components/gallery/NftCard';
+
+const IconStyle = styled(Icon)(({ theme }) => ({
+  width: 20,
+  height: 20,
+  marginTop: 1,
+  flexShrink: 0,
+  marginRight: theme.spacing(2)
+}));
 
 const TabsWrapperStyle = styled('div')(({ theme }) => ({
   zIndex: 9,
@@ -86,7 +113,6 @@ export default function CollectionViewer() {
       <Container maxWidth="lg">
         <Box
           sx={{
-            mb: 3,
             height: 280,
             position: 'relative'
           }}
@@ -94,8 +120,118 @@ export default function CollectionViewer() {
           <ProfileCover />
         </Box>
 
-        <Box sx={{ height: 100 }} />
+        <Box sx={{ height: 64 }} />
+        <Stack alignItems="center">
+          <Typography variant="h3" sx={{ mt: 3, mb: 5 }}>
+            Collection Name
+          </Typography>
+        </Stack>
+        <Grid container spacing={2} sx={{ mb: 3 }}>
+          <Grid item xs={12} md={6}>
+            <Paper
+              key={'123'}
+              sx={{
+                p: 3,
+                width: 1,
+                position: 'relative',
+                border: (theme) => `solid 1px ${theme.palette.grey[500_32]}`
+              }}
+            >
+              <Typography
+                variant="overline"
+                sx={{ mb: 3, display: 'block', color: 'text.secondary' }}
+              >
+                Description
+              </Typography>
+              <Typography variant="body2">
+                The rapid evolution of the blockchain world has forced the One-Eyes to evolve in
+                order to raise an army to fight for the meta universe. The result is the 10,000th
+                EVO Squad - the goal of which is to take over the space for future 3D One-Eyed
+                Avatars. Join our squad . One of the goals of the project is to raise funds to fight
+                childhood cancer. Don't be indifferent. Community links in the profile description .
+              </Typography>
 
+              <Stack spacing={2} sx={{ p: 3 }}>
+                <Stack key="1" direction="row" alignItems="center">
+                  <IconStyle icon={linkedinFill} color="#006097" />
+                  <Link component="span" variant="body2" color="text.primary" noWrap>
+                    https://www.linkedin.com/
+                  </Link>
+                </Stack>
+                <Stack key="1" direction="row" alignItems="center">
+                  <IconStyle icon={twitterFill} color="#1C9CEA" />
+                  <Link component="span" variant="body2" color="text.primary" noWrap>
+                    https://www.linkedin.com/
+                  </Link>
+                </Stack>
+                <Stack key="1" direction="row" alignItems="center">
+                  <IconStyle icon={instagramFilled} color="#D7336D" />
+                  <Link component="span" variant="body2" color="text.primary" noWrap>
+                    https://www.linkedin.com/
+                  </Link>
+                </Stack>
+                <Stack key="1" direction="row" alignItems="center">
+                  <IconStyle icon={facebookFill} color="#1877F2" />
+                  <Link component="span" variant="body2" color="text.primary" noWrap>
+                    https://www.linkedin.com/
+                  </Link>
+                </Stack>
+              </Stack>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Paper
+              sx={{
+                p: 3,
+                width: 1,
+                position: 'relative',
+                border: (theme) => `solid 1px ${theme.palette.grey[500_32]}`
+              }}
+            >
+              <Typography
+                variant="overline"
+                sx={{ mb: 3, display: 'block', color: 'text.secondary' }}
+              >
+                Preview Collection
+              </Typography>
+
+              <Stack spacing={2}>
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                    Standard
+                  </Typography>
+                  <Typography variant="subtitle2">ERC721</Typography>
+                </Stack>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  Features
+                </Typography>
+                <Paper
+                  key={'123'}
+                  sx={{
+                    mb: 3,
+                    p: 0.5,
+                    border: (theme) => `solid 1px ${theme.palette.grey[500_32]}`,
+                    '& > :not(style)': {
+                      m: 0.5
+                    }
+                  }}
+                >
+                  <Chip size="small" label="Burnable"></Chip>
+                  <Chip size="small" label="Enumarable"></Chip>
+                  <Chip size="small" label="Burnable"></Chip>
+                  <Chip size="small" label="Enumarable"></Chip>
+                  <Chip size="small" label="Burnable"></Chip>
+                  <Chip size="small" label="Enumarable"></Chip>
+                  <Chip size="small" label="Burnable"></Chip>
+                  <Chip size="small" label="Enumarable"></Chip>
+                  <Chip size="small" label="Burnable"></Chip>
+                  <Chip size="small" label="Enumarable"></Chip>
+                </Paper>
+                <Stack direction="row" justifyContent="space-between"></Stack>
+              </Stack>
+            </Paper>
+          </Grid>
+        </Grid>
         <Grid container spacing={0}>
           <Stack
             direction="row"
