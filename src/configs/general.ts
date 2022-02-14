a64a4204228da5b8fb0c1e47c7e620f98a849f0d
ttran@iorga.com <ttran>
2022-02-14 20:50:44 +0100

    add interceptor axios

--- src/configs/general.ts
+++ src/configs/general.ts
@@ -1,3 +1,4 @@
 export const NB_NFT_PER_PAGE = 12;
 export const API_POLYGONSCAN_URL = 'https://api.polygonscan.com/api?';
 export const API_KEY_POLYGONSCAN = 'NJY48DSZP5RJ1ERAMKH6ZVHWXUYSQMVCGK';
+export const NB_RETRY_GET_DATA_FROM_TOKEN_URI = 2;
--- src/services/fetchCollection/fetchCollectionPolygonScan.ts
+++ src/services/fetchCollection/fetchCollectionPolygonScan.ts
@@ -1,8 +1,7 @@
 import { INFTCollection } from 'interfaces/collection';
-import { API_POLYGONSCAN_URL, API_KEY_POLYGONSCAN } from 'configs/general';
-
-const axios = require('axios');
-const apiPolygonScanAdress = 'https://api.polygonscan.com/api?';
+import { API_POLYGONSCAN_URL, NB_RETRY_GET_DATA_FROM_TOKEN_URI } from 'configs/general';
+import axios from 'axios';
+import { retryIfRequestError } from 'services/http';
 
 const addNewCollection = (contractAddress: string, finalResult: Array<INFTCollection>) => {
   let newCollection: INFTCollection = {
@@ -28,46 +27,53 @@ const fillCollection = (
 };
 
 export async function getERC271CollectionByAddress(walletaddress: string) {
+  const instance = axios.create();
+  retryIfRequestError(instance, { retry_time: NB_RETRY_GET_DATA_FROM_TOKEN_URI });
   let finalResult: Array<INFTCollection> = [];
   let params = {
     params: {
       module: 'account',
       action: 'tokennfttx',
       address: walletaddress,
-      sort: 'desc',
-      apikey: API_KEY_POLYGONSCAN
+      sort: 'desc'
     }
   };
 
-  await axios.get(API_POLYGONSCAN_URL, params).then((returnData: any) => {
-    const listTransaction = returnData.data.result;
-    for (let i = 0; i < listTransaction.length; i++) {
-      let tokenId = listTransaction[i].tokenID;
-      let from = listTransaction[i].from;
-      let to = listTransaction[i].to.toLowerCase();
-      let contractAddress = listTransaction[i].contractAddress;
-      let contractAddressIndex = -1;
-      if (finalResult.length === 0) {
-        addNewCollection(contractAddress, finalResult);
-        contractAddressIndex = 0;
-      } else {
-        let i = 0;
-        let index = -1;
-        for (i; i < finalResult.length; i++) {
-          if (finalResult[i].contractAddress === contractAddress) {
-            index = i;
-            break;
+  await instance
+    .get(API_POLYGONSCAN_URL, params)
+    .then((returnData: any) => {
+      const listTransaction = returnData.data.result;
+      for (let i = 0; i < listTransaction.length; i++) {
+        let tokenId = listTransaction[i].tokenID;
+        let from = listTransaction[i].from;
+        let to = listTransaction[i].to.toLowerCase();
+        let contractAddress = listTransaction[i].contractAddress;
+        let contractAddressIndex = -1;
+        if (finalResult.length === 0) {
+          addNewCollection(contractAddress, finalResult);
+          contractAddressIndex = 0;
+        } else {
+          let i = 0;
+          let index = -1;
+          for (i; i < finalResult.length; i++) {
+            if (finalResult[i].contractAddress === contractAddress) {
+              index = i;
+              break;
+            }
+          }
+          if (index === -1) {
+            addNewCollection(contractAddress, finalResult);
+            contractAddressIndex = i;
+          } else {
+            contractAddressIndex = index;
           }
         }
-        if (index === -1) {
-          addNewCollection(contractAddress, finalResult);
-          contractAddressIndex = i;
-        } else {
-          contractAddressIndex = index;
-        }
+        fillCollection(finalResult[contractAddressIndex], tokenId, from, to, walletaddress);
       }
-      fillCollection(finalResult[contractAddressIndex], tokenId, from, to, walletaddress);
-    }
-  });
+    })
+    .catch((error: any) => {
+      //TODO check what if error
+      console.log(error);
+    });
   return finalResult;
 }
